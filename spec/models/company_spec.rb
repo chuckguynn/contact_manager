117ded9eff7073b5f7a5e12624dcc247db8eca0d
chuckguynn <chuckguynn@me.com>
2013-11-18 13:43:25 -0600

    add validations for company name

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) do
+    Company.new(name: 'Massive Dynamics')
+  end
+
+    it 'is valid' do
+    expect(company).to be_valid
+  end
+
+    it 'is invalid without a name' do
+    company.name = nil
+    expect(company).to_not be_valid
+  end
 end
